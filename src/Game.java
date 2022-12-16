9f15645704ed441b89d0735b63275397e9340767
Riccardo Roman <98636234+Il-Ricci0@users.noreply.github.com>
2022-12-16 20:27:42 +0100

    +code cleanup

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,7 @@ enum Cell {
     VOID,
     PLAYER,
     WALL
-  }
+}
 
 public class Game {
     public static Integer worldDimensionX = 40;
@@ -13,7 +13,6 @@ public class Game {
     public static Boolean end = false;
     public static Cell[][] world = generateEmptyWorld(worldDimensionX, worldDimensionY);
     public static Player player = new Player("Giocatore 1", 0, 0);
-    static JPanel panel = createPanel();
     public static final String VOID = "___";
     public static final String PLAYER = "| O |";
     // martrix[row][column]
@@ -27,7 +26,7 @@ public class Game {
             public void keyPressed(KeyEvent e) {
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_W:
-                        if(player.getRow() - 1 >= 0){
+                        if (player.getRow() - 1 >= 0) {
                             world[player.getRow()][player.getColumn()] = Cell.VOID;
                             player.setRow(player.getRow() - 1);
                             world[player.getRow()][player.getColumn()] = Cell.PLAYER;
@@ -35,7 +34,7 @@ public class Game {
                         break;
 
                     case KeyEvent.VK_A:
-                        if(player.getColumn() - 1 >= 0){
+                        if (player.getColumn() - 1 >= 0) {
                             world[player.getRow()][player.getColumn()] = Cell.VOID;
                             player.setColumn(player.getColumn() - 1);
                             world[player.getRow()][player.getColumn()] = Cell.PLAYER;
@@ -43,7 +42,7 @@ public class Game {
                         break;
 
                     case KeyEvent.VK_S:
-                        if(player.getRow() + 1 < worldDimensionX){
+                        if (player.getRow() + 1 < worldDimensionX) {
                             world[player.getRow()][player.getColumn()] = Cell.VOID;
                             player.setRow(player.getRow() + 1);
                             world[player.getRow()][player.getColumn()] = Cell.PLAYER;
@@ -51,7 +50,7 @@ public class Game {
                         break;
 
                     case KeyEvent.VK_D:
-                        if(player.getColumn() + 1 < worldDimensionY){
+                        if (player.getColumn() + 1 < worldDimensionY) {
                             world[player.getRow()][player.getColumn()] = Cell.VOID;
                             player.setColumn(player.getColumn() + 1);
                             world[player.getRow()][player.getColumn()] = Cell.PLAYER;
@@ -71,18 +70,18 @@ public class Game {
         // Add the panel to a frame and display it
         JFrame frame = new JFrame();
         frame.add(panel);
-        frame.setSize(750, 700);  // Set the size of the frame
-        frame.setVisible(true);  // Make the frame visible
+        frame.setSize(750, 700); // Set the size of the frame
+        frame.setVisible(true); // Make the frame visible
 
         world[player.getRow()][player.getColumn()] = Cell.PLAYER;
 
-         while (true) {
+        while (true) {
             label.setText(printWorld(world));
 
-             // Update the panel to show the new label color
-             panel.revalidate();
-             panel.repaint();
-         }
+            // Update the panel to show the new label color
+            panel.revalidate();
+            panel.repaint();
+        }
     }
 
     static Cell[][] generateEmptyWorld(Integer worldDimensionX, Integer worldDimensionY) {
@@ -107,52 +106,5 @@ public class Game {
         }
         output += "</html>";
         return output.replaceAll("VOID", VOID).replaceAll("PLAYER", PLAYER);
-    }
-
-    public Integer[] findPlayer(Integer[][] world, Integer worldDimensionX, Integer worldDimensionY) {
-        Integer[] playerPosition = new Integer[2];
-
-        for (int columnIndex = 0; columnIndex < worldDimensionX; columnIndex++) {
-            for (int rowIndex = 0; rowIndex < worldDimensionY; rowIndex++) {
-                if (world[columnIndex][rowIndex] == 1) {
-                    playerPosition[0] = columnIndex;
-                    playerPosition[1] = rowIndex;
-                }
-            }
-        }
-
-        return playerPosition;
-    }
-
-    static JPanel createPanel() {
-        JPanel panel = new JPanel();
-        return panel;
-    }
-
-    public void clearConsole() {
-        try {
-            if (System.getProperty("os.name").contains("Windows"))
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            else
-                Runtime.getRuntime().exec("clear");
-        } catch (Exception e) {
-        }
-    }
-
-    public class GameLoop implements Runnable {
-        private void gameLoop() {
-            while (!end) {
-                refreshScreen();
-            }
-        }
-
-        private void refreshScreen() {
-            clearConsole();
-            printWorld(world);
-        }
-
-        public void run() {
-            gameLoop();
-        }
     }
 }
